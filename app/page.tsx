97ac9ca4fb981fda3dca45f001e7eb6bb5738916
pokopek <pokopek@gmail.com>
2023-11-19 00:33:26 +0700

    halaman home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Hero from "public/hero.png";
+
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          cara desain yang lebih baik lagi di sini
+        </h1>
+        <p className={styles.desc}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia minima
+          nulla quo qui similique, amet dolor repudiandae velit itaque adipisci?
+          Dolores tempore possimus nisi, cum inventore neque non voluptas
+          dignissimos.
+        </p>
+        <button className={styles.button}>See Our Works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="" className={styles.img} />
+      </div>
+    </div>
+  );
 }
